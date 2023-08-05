8a4c9b73c9663ce7e3640083320866804d8eac34
Jonathan Z <141511515+Jonathan-Z-Code@users.noreply.github.com>
2023-08-05 13:03:46 -0400

    Add files via upload

---
+++ C Pointer.c
@@ -0,0 +1,18 @@
+//test code 
+
+/* BLOCK QOUTE */
+
+#include<stdio.h>
+
+int main() {
+  // Checkpoint 1 code goes here. (ADDITIONAL LINEPRINT CMD)
+
+    printf("\n ok this is a test \t questions anyone?");
+
+  // Checkpoint 2 code goes here. test 
+
+  // Checkpoint 3 code goes here.
+  
+  printf("hellllo");
+
+}
